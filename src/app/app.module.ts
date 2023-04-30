35e986378a16fb3e541f254c6b25d2534bb5566d
DianaSolarte <dianasolartem@gmail.com>
2023-04-30 14:20:25 -0500

    creation of app home, table, sidebar and service json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TableComponent } from './table/table.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    TableComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/services/jsonplaceholder.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+//SOLICITUD A LA API HTTP
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JSONPlaceholderService {
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    const url = "https://jsonplaceholder.typicode.com/posts"
+    return this.http.get<any>()
+
+  }
+
+}
+
